3091a63e485cf770a0730603b25d8a45d4cc7f59
Luizdelimaribas <luizdelimaribas@live.com>
2025-01-05 12:33:57 -0300

    start

---
+++ WebScraping.py
@@ -0,0 +1,18 @@
+import requests
+from bs4 import BeautifulSoup
+
+pagina=requests.get('https://myanimelist.net/topanime.php')
+dados_pagina=BeautifulSoup(pagina.text, 'html.parser')
+
+# Encontrar todos os elementos h3 com a classe anime_ranking_h3
+dados_ranking=dados_pagina.find_all('h3', class_='anime_ranking_h3')
+
+# Abrir o arquivo uma vez antes do loop
+with open('C:/Users/Luiz/OneDrive/Documentos/Projetos/Automação de Tarefas no SQL SERVER com Python/nome-animes.txt', 'a') as arquivo:
+   for h3 in dados_ranking:
+     nome_anime=h3.find('a').text
+     arquivo.write('\n{}'.format(nome_anime))
+
+
+
+

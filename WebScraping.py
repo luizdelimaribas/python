897e9bef858b6793340d52b41ba9479ca4b1f9f4
Luizdelimaribas <luizdelimaribas@live.com>
2025-01-05 13:02:57 -0300

    teste1

--- WebScraping.py
+++ WebScraping.py
@@ -1,6 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
-
+#designando a pagina
 pagina=requests.get('https://myanimelist.net/topanime.php')
 dados_pagina=BeautifulSoup(pagina.text, 'html.parser')
 
@@ -12,6 +12,7 @@ with open('C:/Users/Luiz/OneDrive/Documentos/Projetos/Automação de Tarefas no 
    for h3 in dados_ranking:
      nome_anime=h3.find('a').text
      arquivo.write('\n{}'.format(nome_anime))
+
 
 
 
